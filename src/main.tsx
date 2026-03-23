64c91ce034ab4c6538eaec867149f1285e3f55c2
nguyencanbg <nguyencanbg@gmail.com>
2026-03-23 17:57:08 +0700

    Doi ten file thanh app.tsx de sua loi module

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,9 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import { App, ZMPRouter, SnackbarProvider } from "zmp-ui";
-import "zmp-ui/zaui.css"; // Quan trọng để hiển thị giao diện đẹp
-import AppContent from "./App";
+import "zmp-ui/zaui.css";
+// Dòng này cực kỳ quan trọng, phải là app viết thường:
+import AppContent from "./app"; 
 
 const root = createRoot(document.getElementById("app")!);
 root.render(
